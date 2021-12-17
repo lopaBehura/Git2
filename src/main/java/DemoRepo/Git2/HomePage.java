3682ccd5de82da8815fd7c31b5472b6292d22ec1
Lopa <lopabehura@gmail.com>
2021-12-18 01:35:50 +0530

    cherry-pick

--- src/main/java/DemoRepo/Git2/HomePage.java
+++ src/main/java/DemoRepo/Git2/HomePage.java
@@ -1,10 +1,14 @@
 package DemoRepo.Git2;
 
 public class HomePage {
+<<<<<<< HEAD
 	 public void Method1() {
 		   System.out.println("Home Page");
 	   }
 	 public void Method2() {
+=======
+	 public void login() {
+>>>>>>> 07dba04 (branch testing)
 		   System.out.println("Home Page");
 	   }
 }
