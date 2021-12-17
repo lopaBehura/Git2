eef2abcc25a446ebb1ca86fdc233e65537b52657
lopaBehura <68278601+lopaBehura@users.noreply.github.com>
2021-12-17 20:27:50 +0530

    Update LoginPage.java

--- src/main/java/DemoRepo/Git2/LoginPage.java
+++ src/main/java/DemoRepo/Git2/LoginPage.java
@@ -4,4 +4,7 @@ public class LoginPage {
    public void login() {
 	   System.out.println("Login success");
    }
+public void loginDone() {
+	   System.out.println("Login success and logout also");
+   }
 }
